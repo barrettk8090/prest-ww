4366d7a8749332804798d55eeb7f85d1bdff3eda
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-04-27 11:26:50 -0600

    all data except EPS

--- src/components/TableColumns.jsx
+++ src/components/TableColumns.jsx
@@ -7,16 +7,17 @@ function TableColumns({columns}){
           <tr>
             <th>Company</th>
             <th>Ticker</th>
-            <th>Financial Strength</th>
             <th>Current Price</th>
             <th>Current PE Ratio</th>
             <th>Price to Book Value</th>
             <th>Price/Sales Ratio</th>
+            <th>EPS Growth 1-Year</th>
+            <th>EPS Growth 3-Year</th>
             <th>EPS Growth 5-Year</th>
             <th>EPS Growth 10-Year</th>
             <th>Return on Total Capital</th>
             <th>Return on Shareholders' Equity</th>
-            <th>Free Cash Flow</th>
+            <th>Free Cash Flow Growth</th>
             <th>Profit Margin Trail 12 Mo</th>
             <th>Proj 3-5 Yr % Annual Total Return Rank</th>
             <th>PE Current Rank</th>
--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -5,17 +5,18 @@ function TableRow({ stockData }) {
                 <tr key={index}>
                     <td>{stock?.summary?.general?.company}</td>
                     <td>{stock?.summary?.company_data?.symbol}</td>
-                    <td>{stock?.summary?.company_data?.financial_health_grade}</td>
                     <td>{stock?.summary?.company_data?.price}</td>
                     <td>{stock?.summary?.company_data?.pe}</td>
                     <td>{stock?.summary?.company_data?.pb}</td>
                     <td>{stock?.summary?.company_data?.ps}</td>
+                    <td>EPS 1 YR</td>
+                    <td>EPS 3 YR</td>
                     <td>EPS 5 YR</td>
                     <td>EPS 10 YR</td>
                     <td>{stock?.summary?.ratio["ROC (Joel Greenblatt) (%)"].value}</td>
-                    <td>Return Share Equity</td>
-                    <td>Free Cash Flow</td>
-                    <td>Profit MArgin Trial 12</td>
+                    <td>{stock?.summary?.ratio["ROE (%)"].value}%</td>
+                    <td>{stock?.summary?.ratio["FCF Growth (%)"].value}%</td>
+                    <td>{stock?.summary?.ratio["Net-margin (%)"].value}%</td>
                     <td>Proij 3=5 Annual; TOtal</td>
                     <td>PE CUrrent Rank</td>
 
